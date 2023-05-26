6d98eb8aa05527b9f079f7ef2304159fc667d61d
dong959 <binz2028@gmail.com>
2023-05-26 22:20:58 +0700

    edit

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,15 @@ const Header = () => {
     <div class="header-menu">
     
     <ul class="menu-item"></ul>
-    </div>`;
+    </div>
+    <div class="header__menu-icon text-3xl">&#9776;</div>
+    `;
 };
 function renderMenu() {
   const headerMenuItem = document.querySelector(".menu-item");
   const menuItems = menuItem
     .map((item) => {
-      return `<li class="data-menu-${item.id}"><a class="active" src"#"><span>${item.name}</span></a></li>`;
+      return `<li class="data-menu-${item.id} "><a class="active" src"#"><span>${item.name}</span></a></li>`;
     })
     .join("");
   headerMenuItem.innerHTML = menuItems;
